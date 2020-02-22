e40ee4632cf7e67b780dc1795a8c614918eeec17
Peter Kazarinoff <peter.kazarinoff@pcc.edu>
2020-02-21 22:03:49 -0800

    initial add of data, script, script with functions, and tests

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # testing-scientific-code
+
 A repo with example scientific code and tests to go along with it
---
+++ tests/test_plot.py
@@ -0,0 +1,29 @@
+# test_plot.py
+
+import pytest
+import analysis
+import numpy as np
+
+x = np.array([1, 2, 3])
+y = np.array([6, 7, 10])
+fig, ax = analysis.plot(x, y, 'my_title', 'my_x_axis_label', 'my_y_axis_label')
+
+
+def test_tensile_strength():
+    expected = 10.0
+    actual = analysis.get_tensile_strength(y,x)
+    assert round(expected,3) == round(actual,3)
+
+def test_plot_x_axis_label():
+    assert ax.xaxis.label.get_text() == 'my_x_axis_label'
+
+def test_plot_y_axis_label():
+    assert ax.yaxis.label.get_text() == 'my_y_axis_label'
+
+def test_plot_title_label():
+    assert ax.title.get_text() == 'my_title'
+
+def test_get_info():
+    version_dict = analysis.get_info()
+    assert version_dict['Python_major_version']>=3
+    assert version_dict['Python_minor_version']>=6
