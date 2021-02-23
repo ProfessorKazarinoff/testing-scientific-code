653988377e7f9b80dc576a67d2201a18d923f8d1
Peter Kazarinoff <peter.kazarinoff@pcc.edu>
2021-02-23 14:00:09 -0800

    ran tests

--- tests/test_dependencies.py
+++ tests/test_dependencies.py
@@ -1,4 +1,4 @@
-# tests/test_dependancies.py
+# tests/test_dependencies.py
 
 import numpy as np
 import pandas as pd
@@ -6,15 +6,14 @@ import matplotlib as mpl
 
 from pathlib import Path
 
-fp = Path(Path.cwd(), "requirements.txt")
-with open(fp, "r") as f:
+fp = Path(Path.cwd(),'requirements.txt')
+with open(fp,'r') as f:
     v = f.readlines()
-d = {}
+d={}
 for l in v:
     r = l.split("==")[0]
     ver = l.split("==")[1].strip()
-    d[r] = ver
-
+    d[r]=ver
 
 def test_numpy_version():
     expected = d["numpy"]
@@ -29,6 +28,6 @@ def test_pandas_version():
 
 
 def test_matplotlib_version():
-    expected = d["matplotlib"]
-    actual = mpl.__version__
-    assert actual == expected
+     expected = d["matplotlib"]
+     actual = mpl.__version__
+     assert actual == expected
